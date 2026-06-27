273d63989fec065b08ce584f402f9231adb81673
Popov, Kristian <kristian.popov@study.thws.de>
2026-06-27 19:14:46 +0200

    fix: name was corrected

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   
-  // Dynamically set the base path based on the GitHub Actions environment variable
-  base: process.env.GITHUB_PAGES ? '/Haris-Auto/' : '/',
+  base: process.env.GITHUB_PAGES ? '/haris-auto/' : '/',
   
   build: {
     outDir: "dist",
-    assetsInlineLimit: 0, // keep media/fonts as real files
+    assetsInlineLimit: 0, 
   },
 });
